7daaa2871304b49c845a79601eed1bdd3712814d
Kevin Luis Rosario Adorno <kevin.rosario9@upr.edu>
2019-03-29 14:22:15 -0400

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,20 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped" + " " + cardsInPlay[0]);
-
-var cardTwo = cards[3];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped" + " " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 	}
-}
+};
+
+var flipCard = function(cardId){
+	var flippedCard = cards[cardId];
+	cardsInPlay.push(flippedCard);
+	console.log("User flipped" + " " + cards[cardId]);
+};
+
+flipCard(0);
+flipCard(3);
+checkForMatch();
