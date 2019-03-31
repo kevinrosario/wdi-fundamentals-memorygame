5609e0dbe32db4e15b1cccf5d08462917707ab2e
Kevin Luis Rosario Adorno <kevin.rosario9@upr.edu>
2019-03-31 01:27:55 -0400

    Cards are placed randomly

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,20 +23,20 @@ var cards = [
 
 var cardsInPlay = [];
 var score = 0;
+var randArr = [];
 
-var createBoard = function(){
-	for(var i = 0; i < cards.length; i++){
-		var cardElement = document.createElement('img');
-		cardElement.setAttribute('src', "images/back.png");
-		cardElement.setAttribute('data-id', i);
-		cardElement.addEventListener('click', flipCard);
-		document.getElementById('game-board').appendChild(cardElement);
-	}
-};
-
-var gameScore = function(){
-	document.getElementById('game-score').textContent = "Score: " + score;
-};
+var shuffle = function(array) {
+    var i = array.length,
+        j = 0,
+        temp;
+    while (i--) {
+        j = Math.floor(Math.random() * (i+1));
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
 
 var restart = function(){
 	document.getElementById('game-board').innerHTML = "";
@@ -48,6 +48,21 @@ var restartBoard = function(){
 	var restartButton = document.getElementById('restartBtn');
 	restartButton.addEventListener('click', restart);
 
+};
+
+var createBoard = function(){
+	randArr = shuffle([0,1,2,3]);
+	for(var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', randArr[i]);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+var gameScore = function(){
+	document.getElementById('game-score').textContent = "Score: " + score;
 };
 
 var checkForMatch = function(){
@@ -72,6 +87,6 @@ var flipCard = function(){
 	}
 };
 
-createBoard();
 restartBoard();
+createBoard();
 gameScore();
